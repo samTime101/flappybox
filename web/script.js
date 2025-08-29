8989336a0b0bdf71cb421f2aa32a1c2aa0497904
samTime101 <srijanregmi44@gmail.com>
2025-08-29 21:50:56 +0545

    feat: final prototype

--- web/script.js
+++ web/script.js
@@ -8,14 +8,24 @@ async function main(){
     const TOWER_WIDTH = 50
     const TOWER_HEIGHT = 200
     const TOWER_SPACING = 400  
+    let SCORE = 0
 
     let towers = []
     for (let i = 0; i < 5; i++) {
-        towers.push({x:800 +i * TOWER_SPACING,height: Math.random() * (900 - 2 * TOWER_HEIGHT)})
+        towers.push({
+            x:800 +i * TOWER_SPACING,
+            height: Math.random() * (900 - 2 * TOWER_HEIGHT),
+            passed: false
+        })
     }
 
     document.onkeydown = function() {
         yVelocity =  -50 
+    }
+    function write(SCORE){
+        ctx.fillStyle = "black"
+        ctx.font = "30px Arial"
+        ctx.fillText("Score: " + SCORE, 10, 30)
     }
 
     function isColliding(r1,r2) {
@@ -29,6 +39,9 @@ async function main(){
         yVelocity = yVelocity + a * 0.333333
         y = y + yVelocity * 0.333333
         ctx.clearRect(0, 0, 1600, 900)
+
+        write(SCORE)
+
         ctx.beginPath()
         ctx.rect(10, y, 100, 100)
         ctx.fill()
@@ -45,14 +58,22 @@ async function main(){
             tower.x -= 5
             if (tower.x + TOWER_WIDTH < 0) {
                 tower.x = 1600
+                tower.height = Math.random() * (900 - 2 * TOWER_HEIGHT)
+                tower.passed = false
             }
 
             let playerRect = {x:10, y:y, width:100, height:100}
             let topTowerRect = {x:tower.x, y:0, width:TOWER_WIDTH, height:tower.height}
             let bottomTowerRect = {x:tower.x, y:900 - TOWER_HEIGHT, width:TOWER_WIDTH, height:TOWER_HEIGHT} 
-            if (isColliding(playerRect, topTowerRect) || isColliding(playerRect, bottomTowerRect)) {
+
+            if (isColliding(playerRect, topTowerRect) || isColliding(playerRect, bottomTowerRect) || y > 800 || y < 0) {
                 alert("Game Over!")
                 return
+            }
+            if (!tower.passed && tower.x + TOWER_WIDTH < 10) {
+                SCORE += 1
+                tower.passed = true
+                write(SCORE)
             }
 
 
