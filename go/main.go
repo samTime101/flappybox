745d918c49cc74d59d9d87e76534215654f91c0f
samTime101 <srijanregmi44@gmail.com>
2025-09-06 13:23:03 +0545

    changes: to main.go

--- go/main.go
+++ go/main.go
@@ -1,7 +1,14 @@
+// SEPTEMBER 6 2025
+// SAMIP REGMI
+// UTILS.GO
 package main
 
 import "fmt"
 
 func main(){
-	fmt.Println("Hello, World!")
+	canvas := NewCanvas(800, 600)
+	coords := canvas.Coordinates()
+	fmt.Println("Canvas Width:", canvas.Width)
+	fmt.Println("Canvas Height:", canvas.Height)
+	fmt.Println("Canvas Coordinates:", coords)
 }
